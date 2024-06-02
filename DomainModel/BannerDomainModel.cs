fa1fb1fef1548b97135454452b3d95e1953c40c6
jitender012 <jk12101786@gmail.com>
2024-06-02 16:47:54 +0530

    acs

--- DomainModel/BannerDomainModel.cs
+++ DomainModel/BannerDomainModel.cs
@@ -18,4 +18,18 @@ namespace eShop.Domain
         public DateTime? CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
     }
+     public class BannerDomainModel2
+    {
+        public int BannerId { get; set; }
+        public string BannerName { get; set; }
+        public string ImagePath { get; set; }
+        public string Link { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+
+
 }
